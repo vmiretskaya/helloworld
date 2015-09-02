529ebe38f2d02241be36f0ee5240cdf9d2158ee4
Viktoriia Miretska <vmiretskaya@bigmir.net>
2015-09-02 23:09:34 +0200

    02-09-2015-prod_changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901073019) do
+ActiveRecord::Schema.define(version: 20150902210135) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150901073019) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "color"
+    t.float    "price"
   end
 
 end
