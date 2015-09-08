60dd8907232cd243840ed8658fbe6ed40a10c64a
Viktoriia Miretska <vmiretskaya@bigmir.net>
2015-09-08 09:19:40 +0200

    08-09-2015

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,40 @@
 class OrdersController < ApplicationController
 	
-	def index     
-  	end
+	  def index     
+  	 @orders = Order.all
+    end
 
   	def show      
   	end
 
   	def new
+      @order = Order.new
+  	
   	end
 
-  	def create        
+  	def create
+      @order = Order.new(order_params)
+
+      respond_to do |format|
+        if @order.save
+          format.html { redirect_to @order, notice: 'Order was successfully created.' }
+          format.json { render :show, status: :created, location: @order }
+        else
+          format.html { render :new }
+          format.json { render json: @order.errors, status: :unprocessable_entity }
+        end
+      end        
   	end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit(:user_id, :product_id, :total)
+    end
 
 end
