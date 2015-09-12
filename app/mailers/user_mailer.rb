3a5aff8d445518cf23ea2362616e16ae79085c91
Viktoriia Miretska <vmiretskaya@bigmir.net>
2015-09-12 12:49:29 +0200

    12-09-2015

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "vmiretskaya@bigmir.net"
-end
 
-def contact_form(email, name, message)
+  def contact_form(email, name, message)
     @message = message
     mail(:from => email, 
             :to => 'vmiretskaya@bigmir.net', 
             :subject => "A new contact form message from #{name}")
   end
+
+end
+
