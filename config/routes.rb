b77b2218fc90a4e71dc0a8112f8a70d79ccea139
Viktoriia Miretska <vmiretskaya@bigmir.net>
2015-09-24 19:09:04 +0200

    24-09-2015

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   
   resources :users
 
-  resources :products
+  resources :products do
+    resources comments
+  end  
   resources :orders
 
   get 'static_pages/about'
