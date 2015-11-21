93ae5a9780faeef3047cb9874f1d4e7c1e27b14c
Viktoriia Miretska <vmiretskaya@bigmir.net>
2015-11-22 00:59:04 +0100

    22-11-2015

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
 	 protect_from_forgery
+    before_filter :authenticate_user!
     skip_before_action :verify_authenticity_token, if: :json_request?
     respond_to :json, :html
 
